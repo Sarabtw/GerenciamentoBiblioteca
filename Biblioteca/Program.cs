31f50261b97ff6e0052d7c1be21f3b8323b9f764
Sara Monteiro <saramonteiro2008@gmail.com>
2024-10-09 13:39:58 -0300

    Biblioteca

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
-    
+    static LiteralEncoder<Livro> catalogo = new List<Livro>();
+
+    static void Main(string[] args)
+    {
+        CarregarCatalogo();
+
+        while(true)
+    }
 }
